f712fe0873183bfc05f57d2fa089b6f2990da8ae
Bin Son <ndhs1609@gmail.com>
2022-10-18 11:09:03 +0700

    create main com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './compoments/nav-bar/nav-bar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
