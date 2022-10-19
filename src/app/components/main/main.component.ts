65c57e89c9d7e2c8740ca622a95f464b5d791e20
Bin Son <ndhs1609@gmail.com>
2022-10-19 11:29:24 +0700

    data

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  name: string = 'Bin';
+  googleURL: string = 'http://google.com';
+  colSpan = 2;
+  count: number = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onBtnClick() {
+    console.log('Clicked');
   }
 
+  BtnR() {
+    // this.count = this.count - 1;
+    this.count -= 1;
+  }
+  BtnA(event) {
+    console.log('event');
+    this.count = this.count + 1;
+  }
 }
