7f07fe31cfa6ebd16ee4757328245973542ccb3c
Bin Son <ndhs1609@gmail.com>
2022-10-20 16:03:02 +0700

    todo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './compoments/nav-bar/nav-bar.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    MainComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavBarComponent, MainComponent, FooterComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+interface TodoType {
+  id: number;
+  content: string;
+  complete: boolean;
+}
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,19 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   name: string = 'Bin';
+  listTodo: string[] = ['Todo 1', 'Todo 2', 'Todo 3'];
+  listTodoB: TodoType[] = [
+    {
+      id: 1,
+      content: 'todo 1',
+      complete: false,
+    },
+    {
+      id: 2,
+      content: 'todo 2',
+      complete: false,
+    },
+    {
+      id: 3,
+      content: 'todo 3',
+      complete: false,
+    },
+    {
+      id: 4,
+      content: 'todo 4',
+      complete: false,
+    },
+  ];
   googleURL: string = 'http://google.com';
   colSpan = 2;
   count: number = 0;
+  temp: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
   onBtnClick() {
     console.log('Clicked');
   }
+  onClick() {
+    this.listTodo.push(this.name);
+    this.name = '';
+  }
 
   BtnR() {
     // this.count = this.count - 1;
     this.count -= 1;
+  }
+  BtnD(content) {
+    this.listTodoB = this.listTodoB.filter((item) => item.content !== content);
+  }
+  BtnC(id: number) {
+    const Bin = this.listTodoB.findIndex((value) => {
+      return value.id === id;
+    });
+    this.listTodoB[Bin].content = this.listTodoB[Bin].content + ' (complete)';
   }
   BtnA(event) {
     console.log('event');
